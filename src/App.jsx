873b2fa5acc87698cd3cba4b5c546390d278ae77
Vikash Kumar <mrvikashkumar396@gmail.com>
2023-09-14 10:11:58 +0530

    init all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Footer from "./components/footer/Footer";
 import Nav from "./components/Nav";
+import About from "./components/pages/About";
+import ContactUs from "./components/pages/ContactUs";
+import Home from "./components/pages/Home";
+import SignUp from "./components/pages/SignUp";
 import TopMessage from "./components/TopMessage";
 
 function App() {
@@ -7,6 +11,10 @@ function App() {
     <>
       <TopMessage />
       <Nav />
+      <Home />
+      <About />
+      <ContactUs />
+      <SignUp />
       <Footer />
     </>
   );
